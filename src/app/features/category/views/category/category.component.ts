31e6a0ffadd20fee9cdaed99626869fe847c42e6
Micael Oliveira <micaelmoura.oliveira@gmail.com>
2025-03-06 14:54:27 -0300

    Alteração para o category service usar um toSignal

--- src/app/features/category/views/category/category.component.ts
+++ src/app/features/category/views/category/category.component.ts
@@ -1,22 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MainListComponent } from '../../components/main-list/main-list.component';
 import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
-import { CategoryService } from '../../services/category.service';
-import { AsyncPipe } from '@angular/common';
 
 const COMPONENTS = [MainListComponent, ColorsListComponent];
-
-const PIPES = [AsyncPipe];
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [...COMPONENTS, ...PIPES],
+  imports: [...COMPONENTS],
   templateUrl: './category.component.html',
   styles: ``,
 })
-export class CategoryComponent {
-  private readonly categoryService = inject(CategoryService);
-
-  public categories$ = this.categoryService.getCategories();
-}
+export class CategoryComponent {}
