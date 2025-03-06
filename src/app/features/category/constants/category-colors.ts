5a2f2f36c674293534a75e3c2c5dde78e8a8756a
Micael Oliveira <micaelmoura.oliveira@gmail.com>
2025-03-06 14:04:22 -0300

    Componente container categoria

---
+++ src/app/features/category/constants/category-colors.ts
@@ -0,0 +1,15 @@
+export const categoryBackgroundColors: Record<string, string> = {
+  green: 'bg-green-600',
+  orange: 'bg-orange-600',
+  blue: 'bg-blue-600',
+  red: 'bg-red-600',
+  purple: 'bg-purple-600',
+};
+
+export const categoryIdBackgroundColors: Record<string, string> = {
+  '1': 'bg-green-600',
+  '2': 'bg-orange-600',
+  '3': 'bg-blue-600',
+  '4': 'bg-red-600',
+  '5': 'bg-purple-600',
+};
--- src/app/features/category/views/category/category.component.ts
+++ src/app/features/category/views/category/category.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MainListComponent } from '../../components/main-list/main-list.component';
+import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
+import { CategoryService } from '../../services/category.service';
+import { AsyncPipe } from '@angular/common';
+
+const COMPONENTS = [MainListComponent, ColorsListComponent];
+
+const PIPES = [AsyncPipe];
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [...COMPONENTS, ...PIPES],
   templateUrl: './category.component.html',
   styles: ``,
 })
-export class CategoryComponent {}
+export class CategoryComponent {
+  private readonly categoryService = inject(CategoryService);
+
+  public categories$ = this.categoryService.getCategories();
+}
